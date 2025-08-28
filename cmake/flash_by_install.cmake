47101e71d213d4d2714893515614f0f6c1cb8261
rainlong <ylongwangi@qq.com>
2025-08-28 08:49:47 +0800

    fix(install): 修复了烧录文件错误的问题

--- cmake/flash_by_install.cmake
+++ cmake/flash_by_install.cmake
@@ -13,14 +13,23 @@ set(OPENOCD_EXECUTABLE "openocd")
 # halt; program ${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE_NAME}.elf reset\" -c
 # shutdown)")
 
-message(STATUS "Building SLAVE")
-install(
-  CODE CODE
-  "MESSAGE(\"Flash SLAVE......\")"
-  CODE "execute_process(COMMAND openocd -f
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  install(
+    CODE CODE
+    "MESSAGE(\"Flash Debug......\")"
+    CODE "execute_process(COMMAND openocd -f
 ${PROJECT_SOURCE_DIR}/Scripts/OpenOCD/openocd_gdlink.cfg -c \"init; reset
-halt; program ${PROJECT_SOURCE_DIR}/build/debug/wht_slave.elf reset\" -c
+halt; program ${PROJECT_SOURCE_DIR}/build/Debug/wht_slave.elf reset\" -c
 shutdown)")
+else()
+  install(
+    CODE CODE
+    "MESSAGE(\"Flash Release......\")"
+    CODE "execute_process(COMMAND openocd -f
+${PROJECT_SOURCE_DIR}/Scripts/OpenOCD/openocd_gdlink.cfg -c \"init; reset
+halt; program ${PROJECT_SOURCE_DIR}/build/Release/wht_slave.elf reset\" -c
+shutdown)")
+endif()
 
 # # install by bin install( CODE CODE "MESSAGE(\"Flash......\")" CODE
 # "execute_process(COMMAND ${OPENOCD_EXECUTABLE} -f
